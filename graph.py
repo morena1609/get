36992e5fbf2bd21a58c708938ff8fa35a061559c
Марина Ивашкина <ivashkina.ma@phystech.edu>
2022-04-12 17:20:59 +0300

    random_graph

--- graph.py
+++ graph.py
@@ -1,10 +1,12 @@
 import matplotlib.pyplot as plt
+import random
 
-data = [i**2 for i in range(20)]
+data = [random.randint(0, 50) for i in range(20)]
+print(data)
 plt.plot(data)
 data_str = list(map(str, data))
 
 with open('data.txt', 'w') as outp:
     outp.write('\n'.join(data_str))
 
-plt.show() 
+plt.show()
